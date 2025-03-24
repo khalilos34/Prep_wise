30e97f6d34f51d33d78fe8517e965ac5f691b87c
khalilos34 <khalil.naoueji@gmail.com>
2025-03-24 13:41:42 +0100

    auth pages and shadcn

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,13 @@
+import { Toaster } from "sonner";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div> {children}</div>;
+  return (
+    <div className="auth-layout">
+      {" "}
+      {children}
+      <Toaster />
+    </div>
+  );
 };
 
 export default AuthLayout;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return <AuthForm type="sign-up" />;
 };
 
 export default SignupPage;
